5845324d990a157fad9d77c9c52b7b7ab8acf271
bernie6401 <bernie6401@gmail.com>
2022-10-02 00:28:47 +0800

    revise data structure of parseExpression

--- src/header.h
+++ src/header.h
@@ -120,7 +120,7 @@ Declaration parseDeclaration( FILE *source, Token token );
 Declarations *parseDeclarations( FILE *source );
 Expression *parseValue( FILE *source );
 Expression *parseExpressionTail( FILE *source, Expression *lvalue );
-Expression *parseExpression( FILE *source, Expression *lvalue );
+Expression *parseExpression( FILE *source );
 Statement makeAssignmentNode( char id, Expression *v, Expression *expr_tail );
 Statement makePrintNode( char id );
 Statements *makeStatementTree( Statement stmt, Statements *stmts );
