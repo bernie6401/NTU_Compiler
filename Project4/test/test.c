b63a472f9c160e099a24b759f44243e940c30a30
bernie6401 <bernie6401@gmail.com>
2022-12-26 14:51:41 +0800

    add test files

---
+++ Project4/test/test.c
@@ -0,0 +1,12 @@
+int MAIN(){
+    float a = 123.0, b = 12.0;
+    write(a<b);
+    write("\n");
+    write(a>b);
+    write("\n");
+    write(a==b);
+    write("\n");
+    write(a!=b);
+    write("\n");
+    return 0;
+}
